bf1c8d1b196491dd840b8c390ac53ba68d0c406c
Emmanuel-Sasere <emmanuelscript.code@gmail.com>
2025-11-13 13:24:41 +0100

    ADD

--- Javascript.js
+++ Javascript.js
@@ -1,5 +1,7 @@
 // Function Declaration and expression
 
+const { get } = require("http");
+
 //1. Factorial: Write a function declaration and a function expression that compute the factorial of a number. Call both before and after their definitions. Observe the behavior.
 
 //Solution : 
@@ -14,7 +16,7 @@ function factorialDelaration(n){
   }
 }
 
-console.log("Result of Declaration func", factorialDelaration(4)); 
+// console.log("Result of Declaration func", factorialDelaration(4)); 
 
 
 // expression
@@ -29,4 +31,67 @@ let fac = function(n){
 
 }
 
-console.log("Result of Expression func", fac(4));
+// console.log("Result of Expression func", fac(4));
+
+
+//Arrow function
+
+let factorial = (n) => {
+  if (n < 0 ){
+    return "You can't compute factorial for number less than 0";
+  }
+  else if (n === 0){
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+// console.log("Result of Arrow func", factorial(4));
+
+//nested function
+
+function sayHiBye(firstName, lastName){
+  function getFullName(){
+    return firstName + " " + lastName;
+  }
+
+  console.log("Hello, "+ getFullName())
+  console.log("Bye, "+ getFullName());
+  return "";
+
+}
+
+// console.log(sayHiBye("John", "Doe"));
+
+
+function Counter() {
+  let count = 0;
+
+  this.up = function() {
+    return ++count;
+  };
+  this.down = function() {
+    return --count;
+  };
+}
+
+let counter = new Counter();
+
+// console.log( counter.up() ); // ?
+// console.log( counter.up() ); // ?
+// console.log( counter.down() ); // ?
+
+
+
+//Lexical Environment and Closure
+
+function sum(a) {
+  return function(b){
+    return a + b;
+  }
+
+ 
+}
+
+ console.log(sum(4)(5));
